3bb687fefb4665bbdfffa68fe6e17c327927da6a
TheArtOfficial <the.artofficial.trainer@gmail.com>
2025-07-06 12:06:05 -0400

    Create start_aos.bat

---
+++ start_aos.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Remove all containers (running and stopped)
+wsl -e bash -c "docker rm -f $(docker ps -aq)"
+
+:: Pull the latest version of the image
+wsl -e bash -c "docker pull ghcr.io/theartofficial/artofficialstudio:latest"
+
+:: Run the container
+:: Remember to Change /home/theartofficial/comfyvol to the path in your WSL subsystem that you want to use!
+wsl -e bash -c "docker run --gpus all --shm-size=32g -it -p 80:80 -v /home/theartofficial/comfyvol:/workspace ghcr.io/theartofficial/artofficialstudio:latest"
