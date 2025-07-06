6a75f93ab6b9b7db53410a029af7f539c9759071
TheArtOfficial <the.artofficial.trainer@gmail.com>
2025-07-06 11:18:13 -0400

    Update start_comfy.bat

--- start_comfy.bat
+++ start_comfy.bat
@@ -1,1 +1,3 @@
-
+@echo off
+wsl -e bash -c "docker run --gpus all --shm-size=32g -it -p 80:80 -v /home/theartofficial/comfyvol:/workspace ghcr.io/theartofficial/artofficialstudio:latest"
+REM Change /home/theartofficial/comfyvol to the path in your WSL subsystem that you want to use!
