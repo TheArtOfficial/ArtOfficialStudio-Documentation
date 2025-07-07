b62a660920234403f4523224f46e4cc83145c795
TheArtOfficial <the.artofficial.trainer@gmail.com>
2025-07-07 10:49:53 -0400

    Update start_aos.bat

--- start_aos.bat
+++ start_aos.bat
@@ -7,7 +7,7 @@ wsl -e bash -c "docker pull ghcr.io/theartofficial/artofficialstudio:latest"
 
 :: Create the storage location
 :: Remember to Change /home/theartofficial/comfyvol to the path in your WSL subsystem that you want to use!
-wsl -e bash -c "mkdir /home/theartofficial/comfyvol"
+wsl -e bash -c "mkdir -p /home/theartofficial/comfyvol"
 
 :: Run the container
 :: Remember to Change /home/theartofficial/comfyvol to the path in your WSL subsystem that you want to use!
