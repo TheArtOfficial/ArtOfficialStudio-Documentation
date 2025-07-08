261c492a1534788830f3ddece3ca963c8a040db1
TheArtOfficial <the.artofficial.trainer@gmail.com>
2025-07-08 13:19:25 -0400

    Update start_aos.bat

--- start_aos.bat
+++ start_aos.bat
@@ -11,4 +11,4 @@ wsl -e bash -c "mkdir -p /home/theartofficial/comfyvol"
 
 :: Run the container
 :: Remember to Change /home/theartofficial/comfyvol to the path in your WSL subsystem that you want to use!
-wsl -e bash -c "docker run --gpus all --shm-size=32g -it -p 80:80 -v /home/theartofficial/comfyvol:/workspace ghcr.io/theartofficial/artofficialstudio:latest"
+wsl -e bash -c "docker run --gpus all --shm-size=32g -it -p 8080:80 -v /home/theartofficial/comfyvol:/workspace ghcr.io/theartofficial/artofficialstudio:latest"
